388d35976b842cb8d7108151ba223ce250e495d9
Batmoun <ahmed.bn.tnv@gmail.com>
2025-01-28 22:47:27 +0100

    initial

---
+++ js-test/src/Stores/tasks.jsx
@@ -0,0 +1,68 @@
+import {create} from "zustand";
+import axiosInstance from "./axiosInstance";
+
+const initialState = {
+  loading: false,
+  success: false,
+  error: false,
+  data: null,
+  errorData: null,
+};
+
+export const useStoreTasks = create((set) => ({
+  ...initialState,
+  
+  fetchTaskById: async (id) => {
+    set({ ...initialState, loading: true });
+    try {
+      const response = await axiosInstance.get(`/${id}`); // Appel avec l'ID directement
+      set({ ...initialState, success: true, data: response.data });
+    } catch (err) {
+      console.error("Error fetching task:", err);
+      set({ ...initialState, error: true, errorData: err.message });
+    }
+  },
+
+  fetchTasks: async () => {
+    set({ ...initialState, loading: true });
+    try {
+      const response = await axiosInstance.get("/");
+      set({ ...initialState, success: true, data: response.data });
+    } catch (err) {
+      console.error("Error fetching tasks:", err);
+      set({ ...initialState, error: true, errorData: err.message });
+    }
+  },
+
+  addTask: async (task) => {
+    set({ loading: true });
+    try {
+      const response = await axiosInstance.post("/", task);
+      set((state) => ({
+        ...state,
+        data: [...(state.data || []), response.data],
+        success: true,
+        loading: false,
+      }));
+    } catch (err) {
+      console.error("Error adding task:", err);
+      set({ error: true, errorData: err.message, loading: false });
+    }
+  },
+
+  deleteTask: async (id) => {
+    set({ loading: true });
+    try {
+      await axiosInstance.delete(`/${id}`);
+      set((state) => ({
+        ...state,
+        data: state.data?.filter((task) => task.id !== id),
+        success: true,
+        loading: false,
+      }));
+    } catch (err) {
+      console.error("Error deleting task:", err);
+      set({ error: true, errorData: err.message, loading: false });
+    }
+  },
+}));
---
+++ js-test/src/routes/about.jsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/about')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div className='h1'>Noter groupe 6 </div>
+}
